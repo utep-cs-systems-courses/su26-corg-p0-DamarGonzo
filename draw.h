79ad5898927ac8d115e273d0de376e40a4b5469d
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-06-19 15:37:18 -0600

    Add declarations for print_arrow and print_char_8x12
    Added function declaration for print_arrow and print_char_8x12.

--- draw.h
+++ draw.h
@@ -2,8 +2,12 @@
 #define DRAW_INCLUDED
 
 void print_triangle(int startCol, int size);
+// calls out the print arrow
+void print_arrow(int leftCol, int size);
 void print_square(int startCol, int size);
 void print_char_5x7(char c);
+// calls out the print 8x12
+void print_char_8x12(char c);
 
 extern const unsigned char font_5x7[][5];
 

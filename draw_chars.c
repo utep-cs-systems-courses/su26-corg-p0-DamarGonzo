6422ed8148ec58750145df7c78a9dbac94138bf7
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-06-19 15:15:19 -0600

    Add function to print characters in 8x12 font

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,18 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+/* To print the new 8x12 font */
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char col = 0; col < 8; col++) {
+    for (char row = 0; row < 12; row++) {
+      unsigned short rowBits = font_8x12[c][row];
+      unsigned short colMask = 1 << (7-col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
